bc326f1a242cf79c9a8bb35b8100c2009221bb3e
Ulibomber1 <31135793+Ulibomber1@users.noreply.github.com>
2024-07-29 22:35:48 -0700

    Shader Review Comments

--- d3d12EZ-1/DXSupport/Shader.cpp
+++ d3d12EZ-1/DXSupport/Shader.cpp
@@ -2,27 +2,27 @@
 
 Shader::Shader(std::string_view name)
 {
-	static std::filesystem::path shaderDir;
+	static std::filesystem::path shaderDir; // will store the current directory
 	if (shaderDir.empty())
 	{
-		wchar_t moduleFileName[MAX_PATH];
-		GetModuleFileNameW(nullptr, moduleFileName, MAX_PATH);
+		wchar_t moduleFileName[MAX_PATH]; 
+		GetModuleFileNameW(nullptr, moduleFileName, MAX_PATH); // gets the name of the current window module's file
 
 		shaderDir = moduleFileName;
-		shaderDir.remove_filename();
+		shaderDir.remove_filename(); // remove the app.exe at the end
 	}
 
 	std::ifstream shaderIn(shaderDir / name, std::ios::binary);
 	if (shaderIn.is_open())
 	{
-		shaderIn.seekg(0, std::ios::end);
-		m_size = shaderIn.tellg();
-		shaderIn.seekg(0, std::ios::beg);
-		m_data = malloc(m_size);
+		shaderIn.seekg(0, std::ios::end); // seek to eof
+		m_size = shaderIn.tellg(); // this gives the filesize by bytes 
+		shaderIn.seekg(0, std::ios::beg); // seek to bof, to reset the stream
+		m_data = malloc(m_size); // allocate the memory for the binary, get a pointer to that memory back
 
 		if (m_data)
 		{
-			shaderIn.read((char*)m_data, m_size);
+			shaderIn.read((char*)m_data, m_size); // read the data to the allocated memory, byte by byte (hence the (char*))
 		}
 	}
 }
--- d3d12EZ-1/Shaders/PixelShader.hlsl
+++ d3d12EZ-1/Shaders/PixelShader.hlsl
@@ -1,3 +1,4 @@
+// takes an RGBA or other 4-float value and writes it to a render target
 float4 main() : SV_Target
 {
 	return float4(1.0f, 1.0f, 1.0f, 1.0f);
