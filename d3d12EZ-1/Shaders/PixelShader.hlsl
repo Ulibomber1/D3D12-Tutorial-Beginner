81e5ac0168519aa3636350c60f1a7fa0c71c183d
Ulibomber1 <31135793+Ulibomber1@users.noreply.github.com>
2024-08-17 16:33:26 -0700

    Pixel Shader using texel data
    -modified the root signature to store a descriptor table with a Shader Resource View to the Texture2D resource and a Satic Sampler to allow sampling of the texture.
    -modified the pixel shader to sample the texture and return an rgb value in a float4 format

--- d3d12EZ-1/Shaders/PixelShader.hlsl
+++ d3d12EZ-1/Shaders/PixelShader.hlsl
@@ -1,10 +1,13 @@
 #include "RootSignature.hlsl"
 
 float3 color : register(b0);
+Texture2D<float4> textures[] : register(t0);
+sampler textureSampler : register(s0);
 
 [RootSignature(ROOTSIG)]
 // takes an RGBA or other 4-float value and writes it to a render target
 float4 main() : SV_Target
 {
-    return float4(color, 1.0f);
+    float4 texel = textures[0].Sample(textureSampler, float2(0.5f, 0.5f));
+    return float4(texel.rgb, 1.0f);
 }
