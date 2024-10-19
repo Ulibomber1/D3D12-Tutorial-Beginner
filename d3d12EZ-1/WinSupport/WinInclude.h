03eb34068126ec1b76f9bb2556bb3007fbf8fd80
Ulibomber1 <31135793+Ulibomber1@users.noreply.github.com>
2024-10-19 15:53:21 -0700

    Added D3D12 Helper Library

--- d3d12EZ-1/WinSupport/WinInclude.h
+++ d3d12EZ-1/WinSupport/WinInclude.h
@@ -6,7 +6,9 @@
 
 #include <d3d12.h> // DirectX12 Graphics API
 #include <dxgi1_6.h> // DirectX Graphics Infrastructure (latest)
-#include <DirectXMath.h>
+#include <DirectXMath.h> 
+#include <d3dx12.h>
+#include <d3dx12_root_signature.h>
 
 #ifdef _DEBUG // Include debug layer headers, if this is a debug build
 #include <d3d12sdklayers.h>
